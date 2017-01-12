f1cf043c391468bc0bdbc5005f0231a470a305e2
wangrh@99 <wangrh@sdkeji.com>
2017-01-12 21:39:17 +0800

    add publisher

---
+++ mq/pub.go
@@ -0,0 +1,74 @@
+package mq
+
+import (
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/streadway/amqp"
+)
+
+// Publisher a rabbitmq worker in work queues model
+type Publisher struct {
+	Config   Config
+	Exchange string
+}
+
+// Start a new worker
+func (p Publisher) Start(payloadchan chan []byte) {
+	log.Infof("A new publisher on %s", p.Exchange)
+	ch := p.getChannel()
+	defer ch.Close()
+	var err error
+	for payload := range payloadchan {
+		for {
+			err = ch.Publish(
+				p.Exchange, // exchange
+				"",         // routing key
+				false,      // mandatory
+				false,      // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        payload,
+				})
+			if err != nil {
+				log.Error(err, "connection refused,reconnecting...")
+				continue
+			}
+			break
+		}
+	}
+}
+
+func (p Publisher) getChannel() amqp.Channel {
+	for {
+		conn, err = amqp.Dial(p.Config.URL())
+		if err != nil {
+			log.Error(err, "Retry in 2 seconds")
+			time.Sleep(time.Second * 2)
+			continue
+		}
+		log.Info("RabbitMQ connect successful.")
+		// receive channel
+		ch, err = conn.Channel()
+		if err != nil {
+			log.Error(err, "Retry in 2 seconds")
+			time.Sleep(time.Second * 2)
+			continue
+		}
+		err = ch.ExchangeDeclare(
+			p.Exchange, // name
+			"fanout",   // type
+			true,       // durable
+			false,      // auto-deleted
+			false,      // internal
+			false,      // no-wait
+			nil,        // arguments
+		)
+		if err != nil {
+			log.Error(err, "Retry in 2 seconds")
+			time.Sleep(time.Second * 2)
+			continue
+		}
+		return ch
+	}
+}
